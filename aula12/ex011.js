4ddc9e9ba63054986281941317f87e24b98755fb
Ketlyn Moraes Campos <ketlyn.m@aluno.ifsp.edu>
2023-10-12 22:27:50 -0300

    mais

---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 21
+if (idade < 16){
+    condole.log('Não vota')
+} else if(idade < 18 || idade > 65){
+    console.log('voto opcional')
+}else{
+    console.log('Voto obrigatório')
+}
---
+++ aula13/node_7115997b4c1cb.tmp.js
@@ -0,0 +1,13 @@
+var contador = 1
+while(contador <= 10){
+    console.log(contador)
+    contador++
+}
+
+console.log('-----------')
+
+var contador2 = 1
+do{
+    console.log(contador2)
+    contador++
+}while(contador2 <= 10)
